61df616409e8334184b28733e510175a0ba5bf8a
bhacker007 <bharatvasnani007@gmail.com>
2022-04-13 14:36:41 +0100

    final

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let bapi= async(data)=>{
-  await fetch(data)
-  .then(response => {return response.json()})
-  .then(data => console.error(data));
+// Enter number only
+let isEVEN= (number)=>{
+  try{
+    if (number%2==0 && Number.isInteger(number)){
+      return `${number} is even number`
+    }else if(number%2==1 && Number.isInteger(number)){
+      return `${number} is Odd number`
+    }else{
+      throw new Error(`${number} invalid input enter number only`)
+    } 
+  }catch(error){
+     return error;
+  }
 }
 
-module.exports= bapi  
+module.exports= isEVEN
